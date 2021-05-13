070ae3f4ba3b9ce129ca4345298aed53c80d2126
刘桂林 <liu_guilin@qq.com>
2021-05-14 05:18:33 +0800

    0514

--- pkg/buspro_property.py
+++ pkg/buspro_property.py
@@ -4,70 +4,70 @@ from gateway_addon import Property
 
 
 class BusproProperty(Property):
-    """TP-Link property type."""
+  """TP-Link property type."""
 
-    def __init__(self, device, name, description, value):
-        """
-        Initialize the object.
-        device -- the Device this property belongs to
-        name -- name of the property
-        description -- description of the property, as a dictionary
-        value -- current value of this property
-        """
-        Property.__init__(self, device, name, description)
+  def __init__(self, device, name, description, value):
+    """
+    Initialize the object.
+    device -- the Device this property belongs to
+    name -- name of the property
+    description -- description of the property, as a dictionary
+    value -- current value of this property
+    """
+    Property.__init__(self, device, name, description)
 
-        self.set_cached_value(value)
+    self.set_cached_value(value)
 
 
 class OnOffProperty(Property):
-    """TP-Link property type."""
+  """TP-Link property type."""
 
-    def __init__(self, device, name, description, value):
-        """
-        Initialize the object.
-        device -- the Device this property belongs to
-        name -- name of the property
-        description -- description of the property, as a dictionary
-        value -- current value of this property
-        """
-        Property.__init__(self, device, name, description)
+  def __init__(self, device, name, description, value):
+    """
+    Initialize the object.
+    device -- the Device this property belongs to
+    name -- name of the property
+    description -- description of the property, as a dictionary
+    value -- current value of this property
+    """
+    Property.__init__(self, device, name, description)
 
-    def set_value(self, value):
-        if value:
-            self.device.light_device.set_on()
-        else:
-            self.device.light_device.set_off()
+  def set_value(self, value):
+    if value:
+      self.device.light_device.set_on()
+    else:
+      self.device.light_device.set_off()
 
 
 class NumberProperty(Property):
 
-    def __init__(self, device, name, description, value):
-        """
-        Initialize the object.
-        device -- the Device this property belongs to
-        name -- name of the property
-        description -- description of the property, as a dictionary
-        value -- current value of this property
-        """
-        Property.__init__(self, device, name, description)
+  def __init__(self, device, name, description, value):
+    """
+    Initialize the object.
+    device -- the Device this property belongs to
+    name -- name of the property
+    description -- description of the property, as a dictionary
+    value -- current value of this property
+    """
+    Property.__init__(self, device, name, description)
 
-    def set_value(self, value):
-        if value:
-            self.device.set_brightness(value)
+  def set_value(self, value):
+    if value:
+      self.device.set_brightness(value)
 
 
 class StringProperty(Property):
 
-    def __init__(self, device, name, description, value):
-        """
-        Initialize the object.
-        device -- the Device this property belongs to
-        name -- name of the property
-        description -- description of the property, as a dictionary
-        value -- current value of this property
-        """
-        Property.__init__(self, device, name, description)
+  def __init__(self, device, name, description, value):
+    """
+    Initialize the object.
+    device -- the Device this property belongs to
+    name -- name of the property
+    description -- description of the property, as a dictionary
+    value -- current value of this property
+    """
+    Property.__init__(self, device, name, description)
 
-    def set_value(self, value):
-        if value:
-            self.device.set_brightness(value)
+  def set_value(self, value):
+    if value:
+      self.device.set_brightness(value)
